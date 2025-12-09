ec9c2888d8258f1410ea2e8ab9d78a352c76cde9
maegonz <manutonymanu@gmail.com>
2025-12-10 00:25:18 +0100

    Model and methods implementation

---
+++ src/methods.py
@@ -0,0 +1,55 @@
+import torch
+import matplotlib.pyplot as plt
+
+from torch import nn, optim
+from torch.utils.data import DataLoader
+
+def train(model: nn.Module,
+          train_loader: DataLoader,
+          criterion,
+          optimizer: optim.Optimizer,
+          num_epochs: int=20):
+    # 4. Entraînement
+    print("🚀 Entraînement avec Normalisation...")
+    losses = []
+
+    model.train()
+    for epoch in range(num_epochs):
+        total_loss = 0
+        for q, k, y in train_loader:
+            optimizer.zero_grad()
+            preds, _ = model(q, k)
+            loss = criterion(preds, y)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+
+        avg_loss = total_loss / len(train_loader)
+        losses.append(avg_loss)
+
+        # La loss sera maintenant très petite (ex: 0.05) car tout est divisé
+        # Pour avoir l'erreur en Yards, on remultiplie par 10 (l'échelle du label)
+        # MSE -> RMSE -> * 10
+        rmse_yards = (avg_loss**0.5) * 10.0
+
+        print(f"Epoch {epoch+1} | Loss: {avg_loss:.4f} | Erreur estimée: {rmse_yards:.2f} yards")
+
+    plt.plot(losses)
+    plt.title("Loss (Normalisée)")
+    plt.show()
+
+
+def evaluate(model: nn.Module,
+             test_loader: DataLoader,
+             criterion):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for q, k, y in test_loader:
+            preds, _ = model(q, k)
+            loss = criterion(preds, y)
+            total_loss += loss.item()
+
+    avg_loss = total_loss / len(test_loader)
+    rmse_yards = (avg_loss**0.5) * 10.0
+    print(f"Évaluation | Loss: {avg_loss:.4f} | Erreur estimée: {rmse_yards:.2f} yards")
--- src/model.py
+++ src/model.py
@@ -32,3 +32,52 @@ class NFLAttentionModel(nn.Module):
         x = self.dropout(attn_output.squeeze(1)) # Appliquer le dropout après l'attention
         prediction = self.regressor(x)
         return prediction, attn_weights
+    
+
+# class NFLAttentionModel(nn.Module):
+#     def __init__(self, receiver_dim=4, defender_dim=4, embed_dim=64, num_heads=4):
+#         super().__init__()
+
+#         # 1. Embeddings Spécifiques (C'est la nouveauté)
+#         # On projette le Receveur dans l'espace latent
+#         self.receiver_embedding = nn.Linear(receiver_dim, embed_dim)
+
+#         # On projette les Défenseurs dans le MÊME espace latent
+#         self.defender_embedding = nn.Linear(defender_dim, embed_dim)
+
+#         # 2. Multi-Head Attention (Le Cœur)
+#         # batch_first=True pour avoir (Batch, Seq, Features)
+#         self.attention = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
+
+#         # 3. Tête de prédiction (Régression)
+#         # On prédit la "Distance au ballon" (puisque c'est ce qu'on a mis dans le dataset)
+#         # Si la distance prédite est petite, c'est que l'action est un succès.
+#         self.regressor = nn.Sequential(
+#             nn.Linear(embed_dim, 32),
+#             nn.ReLU(),
+#             nn.Linear(32, 2)
+#             # Pas de Sigmoid car on prédit une distance (en yards), pas une proba 0-1
+#         )
+
+#     def forward(self, receiver_data, defenders_data):
+#         # receiver_data: (Batch, 4) -> (Vitesse, Dir, DistBallX, DistBallY)
+#         # defenders_data: (Batch, 11, 4) -> (RelX, RelY, Vitesse, Dir)
+
+#         # A. Projection (Embeddings)
+#         # On ajoute une dimension "Séquence=1" au receveur pour qu'il devienne (Batch, 1, Embed)
+#         query = self.receiver_embedding(receiver_data).unsqueeze(1)
+
+#         # Les défenseurs sont déjà une séquence de 11, donc (Batch, 11, Embed)
+#         keys = self.defender_embedding(defenders_data)
+#         values = keys # Les valeurs sont les mêmes que les clés (Standard)
+
+#         # B. Calcul de l'Attention
+#         # attn_weights sera de forme (Batch, 1, 11)
+#         # C'est TA MÉTRIQUE : L'importance de chaque défenseur
+#         attn_output, attn_weights = self.attention(query, keys, values)
+
+#         # C. Prédiction
+#         # On retire la dimension séquence pour passer dans le régresseur
+#         prediction = self.regressor(attn_output.squeeze(1))
+
+#         return prediction, attn_weights
