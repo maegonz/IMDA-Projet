73c375f0d3982a265893f920169828222e51dfc1
maegonz <manutonymanu@gmail.com>
2025-12-13 11:51:31 +0100

    Remove useless function

--- data/processing.py
+++ data/processing.py
@@ -1,7 +1,4 @@
 import numpy as np
-import pandas as pd
-import glob
-import os
 from typing import Union
 
 
@@ -12,29 +9,6 @@ MAX_ACCEL = 10.0
 MAX_DIST = 50.0
 NUM_FRAMES = 10
 MAX_ANGLE = 360.0
-
-def load_all_data(folder_path):
-    # 1. Charger tous les inputs
-    input_files = glob.glob(os.path.join(folder_path, "input_*.csv"))
-    print(f"Fichiers Input trouvés : {len(input_files)}")
-    df_in_list = [pd.read_csv(f) for f in input_files]
-    if df_in_list:
-        df_in_all = pd.concat(df_in_list, ignore_index=True)
-    else:
-        print("⚠️ Aucun fichier input trouvé !")
-        return None, None
-
-    # 2. Charger tous les outputs
-    output_files = glob.glob(os.path.join(folder_path, "output_*.csv"))
-    print(f"Fichiers Output trouvés : {len(output_files)}")
-    df_out_list = [pd.read_csv(f) for f in output_files]
-    if df_out_list:
-        df_out_all = pd.concat(df_out_list, ignore_index=True)
-    else:
-        df_out_all = pd.DataFrame() # Vide
-
-    return df_in_all, df_out_all
-
 
 def get_angle_features(degree_val):
     rad = np.radians(degree_val)
